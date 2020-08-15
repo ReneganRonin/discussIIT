c5adc1c35c81a671da4f67f24ac85eea0d16c5c8
ReneganRonin <66054069+ReneganRonin@users.noreply.github.com>
2020-08-15 21:00:56 +0800

    Updating new changes
    Just tweaks and fixes

---
+++ users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'last_name', 'first_name', 'is_staff', 'is_authenticated', 'is_active']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
