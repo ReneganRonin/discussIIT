829c7f42cdbeba2a038fc2c99ff9faa35cc85134
ReneganRonin <66054069+ReneganRonin@users.noreply.github.com>
2020-08-16 11:07:34 +0800

    Applying PEP8 changes

--- pages/forms.py
+++ pages/forms.py
@@ -7,5 +7,7 @@ class PostForm(ModelForm):
         model = Post
         fields = ('title', 'body',)
         widgets = {
-            'body': Textarea(attrs={'cols': 80, 'rows': 20, 'placeholder': "Type your post here..."})
+            'body': Textarea(attrs={'cols': 80,
+                                    'rows': 20,
+                                    'placeholder': "Type your post here..."})
         }
